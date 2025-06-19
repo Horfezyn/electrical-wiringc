2edbaad1ed2e9b0a762aa991d105b9044d823109
Jesús Cárdenas <129452845+Horfezyn@users.noreply.github.com>
2025-06-19 17:38:34 -0600

    Updating with 3 CSV files and temp correction factor

--- wiring.c
+++ wiring.c
@@ -7,24 +7,30 @@
 
 // Structures for a conductor
 typedef struct sc_conductor{
-		int sc_gauge_awg_kcmil; // Conductor gauge in AWG/kcmil
-		float sc_ampacity_at_75c_amps; // Ampacity at 75°C conductor temp
+	int sc_gauge_awg_kcmil; // Conductor gauge in AWG/kcmil
+	float sc_ampacity_at_75c_amps; // Ampacity at 75°C conductor temp
 } Conductor;
 
-Conductor g_conductor_data_g_list[20]; // General structure filled with DATA from CSV files, max. 20 types of conductor.
-
-// --- Function Prootypes --- (Under development)
+// Structures for temperature correction factors
+typedef struct s_temp_correction{
+    int stc_ambient_temp;
+    float stc_correction_factor;
+} TempCorrectionFactor;
 
 // For data
 int load_ampacity_table_data(const char *arg_file_name_ptr); // Function to load the ampacity table
+int load_temperature_correction(const char *arg_file_name_ptr); // Function to load the temperature correction factors.
 
 // Calculation
 float calculate_load_current_amps(float arg_power_watts, float arg_voltage_volts, float arg_power_factor, int arg_phase_count); // Initial current calculation
 float calculate_adjusted_current_amps(float arg_load_current_amps, float arg_temp_correction_factor, float arg_num_cond_adjustment_factor); //
 int get_suggested_gauge_awg_kcmil(float arg_adjusted_current_amps); // For simplicity only use the adjusted current
-
+float get_temp_correction_factor(int arg_ambient_temp); // Get the temp correction fator based on ambien temperature.
 // Global variables
+Conductor g_conductor_data_g_list[20]; // General structure filled with DATA from CSV files, max. 20 types of conductor.
 int g_conductor_count = 0; // For the number of conductors in the csv file.
+TempCorrectionFactor g_temp_factors_g_list[20]; // General structure filled with DATA from CSV file, max 20 types of factors
+int g_temp_correction_count = 0; // For the number of correction factors in the CSV file.
 
 // --- Main Function ---
 int main() {
@@ -34,6 +40,7 @@ int main() {
 	float local_power_factor; // Decimal from 0 to 1
 	int local_phase_count; // 1, 2 or 3 phases.
 	float local_circuit_length_meters;
+    int local_ambient_temperature; 
 
 	// Local variables calculated
 	float local_load_current_amps;
@@ -49,6 +56,7 @@ int main() {
     // --- Data loading ---
     printf("--- Loading NOM Data ---\n");
     load_ampacity_table_data("ampacity_data.csv");
+    load_temperature_correction("temp_correction_data.csv");
 
 	// --- User Input ---
 	printf("--- Enter Circuit Parameters ---\n");
@@ -77,6 +85,11 @@ int main() {
         scanf("%f", &local_circuit_length_meters);
     }
     while (local_circuit_length_meters <= 0); //Validate that phase count is a positive number
+    do{
+        printf("Enter ambient temperature (celsius, e.g., 30): ");
+        scanf("%f", &local_ambient_temperature);
+    }
+    while (local_circuit_length_meters <= 0); //Validate that phase count is a positive number
 
 	// --- Calculations ---
 	printf("\n--- Performing Calculations ---\n");
@@ -89,7 +102,7 @@ int main() {
     printf("Calculated Load Current (Ib): %.2f Amps\n", local_load_current_amps);
 
     // Correction factors
-    local_temp_correction_factor = 1.0; // Assuming 30C ambient, no corrction
+    local_temp_correction_factor = get_temp_correction_factor(local_ambient_temperature); // Calculating the correction factor.
     local_num_cond_adjustment_factor = 0.8; // Assuming 3 conductors, 80% adjustment (from NOM 310-15(B)(3)(a))
 
     local_adjusted_current_amps = calculate_adjusted_current_amps(local_load_current_amps, local_temp_correction_factor, local_num_cond_adjustment_factor);
@@ -117,7 +130,7 @@ int main() {
 
 // --- Function Implementations (The hard part) ---
 
-// Data loading
+// Ampacity CSV File
 int load_ampacity_table_data(const char *arg_file_name_ptr) {
     FILE *file_ptr = fopen(arg_file_name_ptr,"r");
     if (!file_ptr){
@@ -125,18 +138,20 @@ int load_ampacity_table_data(const char *arg_file_name_ptr) {
         return 1;
     }
 
-    char line[256]; //Limit for each line, maybe i need less.
+    char line[32]; //Limit for each line, maybe i need less.
     fgets(line, sizeof(line),file_ptr);
 
     g_conductor_count = 0; // Auxiliar for # of conductors to load.
     while (fgets(line, sizeof(line), file_ptr) && g_conductor_count < 30){
         char *token;
         token = strtok(line, ",");
-        if (token) g_conductor_data_g_list[g_conductor_count].sc_gauge_awg_kcmil = atoi(token); // For 1/0 i use 0 as name, for 2/0 -1, for 3/0 -2, and for 4/0 -3 in the CSV, to simplify the comparison
-
+        if (token) {
+            g_conductor_data_g_list[g_conductor_count].sc_gauge_awg_kcmil = atoi(token); // For 1/0 i use 0 as name, for 2/0 -1, for 3/0 -2, and for 4/0 -3 in the CSV, to simplify the comparison
+        }
         token = strtok(NULL, ",");
-        if (token) g_conductor_data_g_list[g_conductor_count].sc_ampacity_at_75c_amps = atof(token);
-
+        if (token) {
+            g_conductor_data_g_list[g_conductor_count].sc_ampacity_at_75c_amps = atof(token);
+        }
         g_conductor_count++;
 
     }
@@ -145,6 +160,32 @@ int load_ampacity_table_data(const char *arg_file_name_ptr) {
     return 0;
 }
 
+// Temperature Corection Factor CSV File
+int load_temperature_correction(const char *arg_file_name_ptr){
+    FILE *file_ptr = fopen(arg_file_name_ptr, "r");
+    if (!file_ptr){
+        printf("Error opening temp_correction_data.csv");
+        return 1;
+    }
+    char line[32]; // Limit for each line.
+    fgets(line, sizeof(line), file_ptr);
+    g_temp_correction_count = 0;
+    while(fgets(line, sizeof(line), file_ptr) && g_temp_correction_count < 20){ // 20 is the max number of correction factors
+        char *token;
+        token = strtok(line, ",");
+        if (token){
+            g_temp_factors_g_list[g_temp_correction_count].stc_ambient_temp = atoi(token);
+        }
+        token = strtok(NULL, "\n");
+        if (token){
+            g_temp_factors_g_list[g_temp_correction_count].stc_correction_factor = atof(token);
+        }
+        g_temp_correction_count++;
+    }
+    fclose(file_ptr);
+    printf("Action: Loaded %d temperature correction factors from %s.\n", g_temp_correction_count, arg_file_name_ptr);
+    return 0;
+}
 // Current calculation
 float calculate_load_current_amps(float arg_power_watts, float arg_voltage_volts, float arg_power_factor, int arg_phase_count){
     if ( arg_voltage_volts == 0 || arg_power_factor == 0) {
@@ -179,4 +220,14 @@ int get_suggested_gauge_awg_kcmil(float arg_adjusted_current_amps){
         }
     }
     return 0;
+}
+
+// Temperature correction factors based on the ambient temp
+float get_temp_correction_factor(int arg_ambient_temp){
+    for (int i = 0; i < g_temp_correction_count; i++){
+        if (g_temp_factors_g_list[i].stc_ambient_temp == arg_ambient_temp){
+            return g_temp_factors_g_list[i].stc_correction_factor;
+        }
+    }
+    return 1; 
 }
